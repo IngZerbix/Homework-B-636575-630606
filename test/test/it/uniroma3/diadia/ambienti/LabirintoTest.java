a5fd0433b5b12fe4748c78bf1bc58360ff6222ea
Stefano Zerbino <ste.zerbino@stud.uniroma3.it>
2026-05-04 18:17:49 +0200

    Refactor LabirintoTest formatting and structure

--- test/test/it/uniroma3/diadia/ambienti/LabirintoTest.java
+++ test/test/it/uniroma3/diadia/ambienti/LabirintoTest.java
@@ -4,9 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import it.uniroma3.diadia.ambienti.Labirinto;
-import it.uniroma3.diadia.ambienti.Stanza;
-
 import org.junit.jupiter.api.BeforeEach;
 
 class LabirintoTest {
