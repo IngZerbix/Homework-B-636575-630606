e4a6e18d37604b37688725f635e0d60299aff447
Stefano Zerbino <ste.zerbino@stud.uniroma3.it>
2026-05-04 19:22:45 +0200

    Format IOConsole methods for better readability

--- src/it/uniroma3/diadia/IOConsole.java
+++ src/it/uniroma3/diadia/IOConsole.java
@@ -2,12 +2,15 @@ package it.uniroma3.diadia;
 import java.util.Scanner;
 
 public class IOConsole implements IO {
-	private Scanner scanner = new Scanner(System.in);  
+        private Scanner scanner = new Scanner(System.in);
 
-    @Override
-    public void mostraMessaggio(String messaggio) { System.out.println(messaggio); }
+        @Override
+        public void mostraMessaggio(String messaggio) {
+                System.out.println(messaggio);
+        }
 
-    @Override
-    public String leggiRiga() { return scanner.nextLine(); }
-
+        @Override
+        public String leggiRiga() {
+                return scanner.nextLine();
+        }
 }
